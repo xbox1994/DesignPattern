1045250144736a83dacc75dcb6f6180a17e00491
Tianyi Wang <tywang666@gmail.com>
2018-06-15 13:22:27 +0800

    build jar file by gradle

--- src/Other/ssh/JavaSSHTest.java
+++ src/Other/ssh/JavaSSHTest.java
@@ -1,21 +1,18 @@
 package Other.ssh;
 
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 public class JavaSSHTest {
 
     public static void main(String[] args) throws Exception {
-        String command = new String(Files.readAllBytes(Paths.get("test.sh")), StandardCharsets.UTF_8);
-        System.out.println(command);
-        String userName = "ubuntu";
-        String password = "ubuntu";
-        String connectionIP = "13.250.14.139";
+        String command = args[3];
+        System.out.println("command: " + command);
+
+        String userName = args[0];
+        String password = args[2];
+        String connectionIP = args[1];
         SSHManager instance = new SSHManager(userName, password, connectionIP);
         SSHOutput output = instance.sendCommand(command).orElseThrow(Exception::new);
-        System.out.println(output.getStdout());
-        System.err.println(output.getStderr());
+        System.out.println("stdout: " + output.getStdout());
+        System.err.println("stderr: " + output.getStderr());
         instance.close();
     }
 }
